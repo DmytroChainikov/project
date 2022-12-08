c77d7c61a5b488a18e1e1f1de892811d4aae890a
DmytroChainikov <dmytro.chainikov@oa.edu.ua>
2022-12-08 04:12:53 +0200

    1

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,13 @@ import fastapi as _fastapi
 from fastapi.middleware.cors import CORSMiddleware
 import fastapi.security as _security
 
-import sqlalchemy.orm as _orm
+import sqlalchemy.orm as _orm, sqlalchemy as _sql
 import database as _database
 import services as _services, schemas as _schemas, models as _models
 
 app = _fastapi.FastAPI()
 
-#_services.create_database()
+# _services.create_database()
 
 origins = ["*"]
 
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.post("/api/users", tags=["users"])
 async def create_user(
@@ -51,11 +52,15 @@ async def generate_token(
 async def get_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
     return user
 
+
 @app.get("/api", tags=["name"])
 async def root():
     return {"message": "Save your money"}
 
-@app.post("/money/{user_id}/add_money/", response_model=_schemas.Money_type, tags=["money"])
+
+@app.post(
+    "/money/{user_id}/add_money/", response_model=_schemas.Money_type, tags=["money"]
+)
 def add_money(
     user_id: int,
     post: _schemas.Money_type_add,
@@ -66,17 +71,44 @@ def add_money(
         raise _fastapi.HTTPException(
             status_code=404, detail="sorry this user does not exist"
         )
-    
+
     return _services.add_money(db=db, post=post, user_id=user_id)
+
+
 @app.get("/money/{user_id}/get_balance", tags=["money"])
-def get_balance(
-    user_id: int,
-    db: _orm.Session = _fastapi.Depends(_services.get_db)
-):
-    post = (_services.get_balance(db=db, user_id=user_id))
+def get_balance(user_id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    print(_models.Money_type.type_quantity)
+    post = _services.get_balance(db=db, user_id=user_id)
     return post
 
-@app.put("/money/{user_id}/edit_money_type", tags=["money"] , response_model=_schemas.Money_type)
+
+@app.get("/money/{user_id}/get_total_balance", tags=["working on"])
+def get_total_balance(
+    user_id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    result = _services.get_total_balance(db=db, user_id=user_id)
+    
+    if result[user_id] is True:
+        raise _fastapi.HTTPException(
+            status_code=404, detail="sorry this user does not exist"
+        )
+    print(result)
+    return result
+
+
+@app.get("/money/{user_id}/get_money_type_id", tags=["money"])
+def get_money_type_id(
+    user_id: int, id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    post = _services.get_money_type_id(db=db, user_id=user_id, id=id)
+    return post
+
+
+@app.put(
+    "/money/{user_id}/edit_money_type",
+    tags=["money"],
+    response_model=_schemas.Money_type,
+)
 def update_type_info(
     id: int,
     user_id: int,
@@ -85,7 +117,30 @@ def update_type_info(
 ):
     return _services.update_type_info(db=db, post=post, id=id, user_id=user_id)
 
-@app.post("/categoty/{user_id}/add_category/", tags=["category"], response_model=_schemas.Category_type)
+
+@app.delete("/money/{user_id}/money_type_delete")
+def delete_money_type(
+    id: int, user_id: int, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    _services.delete_money_type(db=db, id=id, user_id=user_id)
+    db_user = _services.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise _fastapi.HTTPException(
+            status_code=404, detail="sorry this user does not exist"
+        )
+    money_type_id = _services.get_money_type_id(db=db, user_id=user_id, id=id)
+    if money_type_id is None:
+        raise _fastapi.HTTPException(
+            status_code=404, detail="sorry this money type does not exist"
+        )
+    return {"message": f"successfully deleted money type"}
+
+
+@app.post(
+    "/categoty/{user_id}/add_category/",
+    tags=["category"],
+    response_model=_schemas.Category_type,
+)
 def add_category(
     user_id: int,
     post: _schemas.Category_add,
@@ -96,18 +151,27 @@ def add_category(
         raise _fastapi.HTTPException(
             status_code=404, detail="sorry this user does not exist"
         )
-    return _services.add_category(db=db, post=post, user_id=user_id)  
+    return _services.add_category(db=db, post=post, user_id=user_id)
 
-@app.post("/category/{user_id}/add_category_money/", response_model=_schemas.Category_quantity, tags=["category"])
+
+@app.post(
+    "/category/{user_id}/add_category_money/",
+    response_model=_schemas.Category_quantity,
+    tags=["category"],
+)
 def add_category_money(
     category_type_id: int,
     user_id: int,
     post: _schemas.Category_add_money,
     db: _orm.Session = _fastapi.Depends(_services.get_db),
 ):
-    category_type_id = (db.query(_models.Category_type).filter(_models.Category_type.id == category_type_id).first()).__dict__["id"]
+    category_type_id = (
+        db.query(_models.Category_type)
+        .filter(_models.Category_type.id == category_type_id)
+        .first()
+    ).__dict__["id"]
     db_user = _services.get_user(db=db, user_id=user_id)
-    
+
     if category_type_id is None:
         raise _fastapi.HTTPException(
             status_code=404, detail="sorry this category does not exist"
@@ -115,7 +179,10 @@ def add_category_money(
     if db_user is None:
         raise _fastapi.HTTPException(
             status_code=404, detail="sorry this user does not exist"
+        )
+    return _services.add_category_money(
+        db=db, post=post, category_type_id=category_type_id, user_id=user_id
     )
-    return _services.add_category_money(db=db, post=post, category_type_id=category_type_id, user_id=user_id)  
 
-#need fix in add money to category
+
+# need fix in add money to category
--- backend/services.py
+++ backend/services.py
@@ -3,7 +3,7 @@ import fastapi as _fastapi
 import fastapi.security as _security
 import jwt as _jwt
 import datetime as _dt
-import sqlalchemy.orm as _orm
+import sqlalchemy.orm as _orm, sqlalchemy as _sql
 import passlib.hash as _hash
 import database as _database, models as _models, schemas as _schemas
 
@@ -23,11 +23,18 @@ def get_db():
     finally:
         db.close()
 
+
 def get_user(db: _orm.Session, user_id: int):
     return db.query(_models.User).filter(_models.User.id == user_id).first()
 
+
 def get_category(db: _orm.Session, category_type_id: int):
-    return db.query(_models.Category_type).filter(_models.Category_type.id == category_type_id).first()
+    return (
+        db.query(_models.Category_type)
+        .filter(_models.Category_type.id == category_type_id)
+        .first()
+    )
+
 
 async def get_user_by_email(email: str, db: _orm.Session):
     return db.query(_models.User).filter(_models.User.email == email).first()
@@ -77,9 +84,34 @@ async def get_current_user(
 
     return _schemas.User.from_orm(user)
 
-def get_balance(db: _orm.Session, user_id: int):
-    choice_list = db.query(_models.Money_type).filter(_models.Money_type.user_id == user_id).all()
+
+def get_balance(db: _orm.Session, user_id: int, id: int):
+    choice_list = (
+        db.query(_models.Money_type).filter(_models.Money_type.user_id == user_id).all()
+    )
+    value_list: float = (
+        db.query(_models.Money_type.type_quantity)
+        .filter(_models.Money_type.user_id == user_id)
+        .all()
+    )
     return choice_list
+
+def get_total_balance(user_id: int, db: _orm.Session):
+    value_sum = _sql.select(
+        [_models.Money_type.user_id == user_id, _sql.func.sum(_models.Money_type.type_quantity)]
+    ).group_by(_models.Money_type.user_id == user_id)
+    result = _database.engine.execute(value_sum).fetchall()
+    for item in result:
+        print(item[0], item[1])
+    return result
+
+def get_money_type_id(db: _orm.Session, id: int, user_id: int):
+    money_type_id = (
+        db.query(_models.Money_type, user_id)
+        .filter(_models.Money_type.id == id)
+        .first()
+    )
+    return money_type_id
 
 
 def add_money(db: _orm.Session, post: _schemas.Money_type_add, user_id: int):
@@ -89,15 +121,26 @@ def add_money(db: _orm.Session, post: _schemas.Money_type_add, user_id: int):
     db.refresh(post)
     return post
 
-def update_type_info(db: _orm.Session, user_id: int, id: int, post: _schemas.Money_type_add):
-    
-    db_post = get_balance(db=db, id=id)
+
+def update_type_info(
+    db: _orm.Session, user_id: int, id: int, post: _schemas.Money_type_add
+):
+
+    db_post = get_balance(db=db, id=id, user_id=user_id)
     db_post.type_name = post.type_name
     db_post.description = post.type_description
     db_post.quantity = post.type_quantity
     db.commit()
     db.refresh(db_post)
     return db_post
+
+
+async def delete_money_type(db: _orm.Session, id: int, user_id: int):
+    await db.query(_models.Money_type, user_id).filter(
+        _models.Money_type.id == id
+    ).delete()
+    db.commit()
+
 
 def add_category(db: _orm.Session, post: _schemas.Category_add, user_id: int):
     post = _models.Category_type(**post.dict(), user_id=user_id)
@@ -106,11 +149,33 @@ def add_category(db: _orm.Session, post: _schemas.Category_add, user_id: int):
     db.refresh(post)
     return post
 
-def add_category_money(db: _orm.Session, post: _schemas.Category_add_money, category_type_id:int, user_id: int):
-    category_type_id=(db.query(_models.Category_type).filter(_models.Category_type.id == category_type_id).first()).__dict__["id"]
-    post = _models.Category_quantity(**post.dict(), category_type_id=category_type_id, user_id=user_id)
+
+def add_category_money(
+    db: _orm.Session,
+    post: _schemas.Category_add_money,
+    category_type_id: int,
+    user_id: int,
+):
+    category_type_id = (
+        db.query(_models.Category_type)
+        .filter(_models.Category_type.id == category_type_id)
+        .first()
+    ).__dict__["id"]
+    post = _models.Category_quantity(
+        **post.dict(), category_type_id=category_type_id, user_id=user_id
+    )
     print(category_type_id)
     db.add(post)
     db.commit()
     db.refresh(post)
     return post
+
+
+def calc_add_category_money(
+    db: _orm.Session, post: _schemas.Money_type_add, type_id: int
+):
+    get_money = (
+        db.query(_models.Money_type)
+        .filter(_models.Money_type.type_quantity == user_id)
+        .all()
+    )
